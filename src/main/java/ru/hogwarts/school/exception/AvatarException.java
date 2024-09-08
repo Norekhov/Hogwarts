bc67934923a7f26cbe81012a7a1d313ea70318b2
Norekhov <156809648+Norekhov@users.noreply.github.com>
2024-09-08 14:03:46 +0300

    AvatarException

--- src/main/java/ru/hogwarts/school/exception/AvatarException.java
+++ src/main/java/ru/hogwarts/school/exception/AvatarException.java
@@ -2,7 +2,9 @@ package ru.hogwarts.school.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-
+/**
+*Create a class to throw an exception when adding an avatar
+*/
 public class AvatarException extends RuntimeException{
     public ResponseEntity<String> handleAvatarException() {
         return ResponseEntity
