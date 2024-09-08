f21dcc021a52b2fca3bf32186de37d369a1c4c21
Norekhov <156809648+Norekhov@users.noreply.github.com>
2024-09-08 14:13:38 +0300

    FacultyRepository

--- src/main/java/ru/hogwarts/school/repository/FacultyRepository.java
+++ src/main/java/ru/hogwarts/school/repository/FacultyRepository.java
@@ -4,11 +4,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import ru.hogwarts.school.model.Faculty;
 
 import java.util.List;
-
+/**
+*Creating a repository for working with faculties
+*/
 public interface FacultyRepository extends JpaRepository<Faculty, Long> {
-
+/**
+*Repository for obtaining faculty by color
+*/
     List<Faculty> getFacultyByColor(String color);
-
+/**
+*Repository for obtaining a faculty name or color without taking into account registration
+*/
     List<Faculty> getFacultyByColorIgnoreCaseOrNameIgnoreCase(String color, String name);
 
 }
